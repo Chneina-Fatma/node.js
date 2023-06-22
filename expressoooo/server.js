5fdbd994271677c0ea19d860868a1d22665c5d84
chneina2fatma <127303941+chneina2fatma@users.noreply.github.com>
2023-06-22 11:48:16 +0200

    Delete expressoooo directory

--- expressoooo/server.js
+++
@@ -1,7 +0,0 @@
-
-const express=require("express");
-
-const app=express()
-app.get("/user",(req,res)=>res.send("welcome"));
-
-app.listen(1996,(err)=>err?console.log(err): console.log("server is run"))
